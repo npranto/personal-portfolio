df53cf8ab0abdaecc2381b232816b7e35fec21a1
Nazmuz Shakib Pranto <npranto@gmail.com>
2017-04-17 16:54:08 -0400

    adds education component and adds it as a route inside app ccomponent

---
+++ src/app/pages/education/Education.component.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+import './Education.component.css';
+
+class Education extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <h1> Education Component </h1>
+        )
+    }
+}
+
+export default Education;
