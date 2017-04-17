5bb90ccb0f53583dfb7504aebf812355f29ca722
Nazmuz Shakib Pranto <npranto@gmail.com>
2017-04-17 16:37:56 -0400

    adds connect component and adds it as a route inside app ccomponent

--- src/app/App.component.jsx
+++ src/app/App.component.jsx
@@ -7,6 +7,7 @@ import Home from './pages/home/Home.component.jsx';
 import About from './pages/about/About.component.jsx';
 import Skills from './pages/skills/Skills.component.jsx';
 import Projects from './pages/projects/Projects.component.jsx';
+import Connect from './pages/connect/Connect.component.jsx';
 
 class App extends Component{
     render() {
@@ -21,6 +22,7 @@ class App extends Component{
                         <Route path="/about" component={About}/>
                         <Route path="/skills" component={Skills} />
                         <Route path="/projects" component={Projects} />
+                        <Route path="/connect" component={Connect} />
                     </Switch>
                 </section>
             </div>
---
+++ src/app/pages/connect/Connect.component.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+import './Connect.component.css';
+
+class Connect extends Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return (
+            <h1> Connect Component </h1> 
+        )
+    }
+}
+
+export default Connect;
