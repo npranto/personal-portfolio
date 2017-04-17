d397cda3b71f8331104d720c189865f637827733
Nazmuz Shakib Pranto <npranto@gmail.com>
2017-04-17 00:10:51 -0400

    updates package.json and README.md to provide details according to personal-portfolio

--- src/app/App.component.jsx
+++ src/app/App.component.jsx
@@ -6,9 +6,7 @@ class App extends Component{
     render() {
         return (
             <div id="app-component">
-                <h1 style={{textAlign: 'center'}}>
-                    React, ready to go!
-                </h1>
+                
             </div>
         )
     }
