7beeaf76192826e1cd1a941fbf564c9ca9744a74
Nazmuz Shakib Pranto <npranto@gmail.com>
2017-04-17 16:21:22 -0400

    adds react-router-dom for routing our app pages; also adds home and about components

---
+++ src/app/pages/home/Home.component.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+import './Home.component.css';
+
+class Home extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <h1> Home Component </h1> 
+        )
+    }
+}
+
+export default Home;
