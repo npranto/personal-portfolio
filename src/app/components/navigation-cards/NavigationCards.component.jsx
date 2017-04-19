6a5c639e76f3f38815f43721d348203268754295
Nazmuz Shakib Pranto <npranto@gmail.com>
2017-04-19 12:32:23 -0400

    completes portrait-jumbotron component and starts navigation-cards component

---
+++ src/app/components/navigation-cards/NavigationCards.component.jsx
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+import './NavigationCards.component.scss';
+
+class NavigationCards extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div className="navigation-cards-component">
+                NavigationCards
+            </div>
+        )
+    }
+}
+
+export default NavigationCards;
