dddc3d8e549d2c0a30ff9e3900826c62e4b640e5
Nazmuz Shakib Pranto <npranto@gmail.com>
2017-04-22 10:10:09 -0400

    makes card overflow inner content for project cards

--- src/app/pages/projects/Projects.component.jsx
+++ src/app/pages/projects/Projects.component.jsx
@@ -26,8 +26,11 @@ class Projects extends Component {
                         <div className="card-content">
                             <h4> <Link to="/"> Deed </Link> </h4>
                             <p>
-                                A customized slack bot integration and an application that connects recruiters and candidates to
-                                fill technical positions through the slack app.
+                                A mini social networking app that anyone can use to share their good deeds throughout the day.
+                                Users will be able to post, share and be inspired by other's deeds on their home feed. Deed is an
+                                user generated content based app that aims to break a single circle of friends and their stories,
+                                rather opens new perspectives from new individuals everyday to create a positive outlook for the
+                                world around us.
                             </p>
                         </div>
                     </div>
