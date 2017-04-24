0fd7591e38adb011a33e0a29ff317461c63661e8
Nazmuz Shakib Pranto <npranto@gmail.com>
2017-04-24 14:44:34 -0400

    adds 'renderIndividualProjectRoutes' function to render project routes element functionally

--- src/app/App.component.jsx
+++ src/app/App.component.jsx
@@ -3,6 +3,7 @@ import {Route, Switch} from 'react-router-dom';
 
 import './App.component.scss';
 
+import {APP_STORE} from './../assets/store/index.js';
 import NavigationBar from './components/navigation-bar/NavigationBar.component.jsx';
 import SocialsFooter from './components/socials-footer/SocialsFooter.component.jsx';
 import Home from './pages/home/Home.component.jsx';
@@ -19,10 +20,19 @@ class App extends Component{
         super(props);
 
         this.state = {
-
+            projects: APP_STORE.projects
         }
     }
     render() {
+
+        const  renderIndividualProjectRoutes = () => {
+            return this.state.projects.map((project, index)=>{
+                return (
+                    <Route key={index} path={`/project@${project.projectTitle}`} component={Project} />
+                )
+            })
+        }
+
         return (
             <div className="app-component">
                 <section className="header col s12 m12 l12">
@@ -37,10 +47,7 @@ class App extends Component{
                         <Route path="/skills" component={Skills} />
                         <Route path="/education" component={Education} />
                         <Route path="/connect" component={Connect} />
-                        <Route path="/project@Deed" component={Project} />
-                        <Route path="/project@RecruiterBot" component={Project} />
-                        <Route path="/project@WeatherArt" component={Project} />
-                        <Route path="/project@Video5" component={Project} />
+                        {renderIndividualProjectRoutes()}
                     </Switch>
                 </section>
                 <section className="footer col s12 m12 l12">
