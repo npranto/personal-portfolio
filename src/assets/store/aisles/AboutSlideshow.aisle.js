0d1589e1dd2560a59886cf0b9366e2bfda1d43b9
Nazmuz Shakib Pranto <npranto@gmail.com>
2017-05-10 14:17:46 -0400

    updates slideshow images and extensions

--- src/assets/store/aisles/AboutSlideshow.aisle.js
+++ src/assets/store/aisles/AboutSlideshow.aisle.js
@@ -1,9 +1,9 @@
-import slide1 from './../../images/about-slideshow/1.png';
-import slide2 from './../../images/about-slideshow/2.png';
-import slide3 from './../../images/about-slideshow/3.png';
-import slide4 from './../../images/about-slideshow/4.png';
-import slide5 from './../../images/about-slideshow/5.png';
-import slide6 from './../../images/about-slideshow/6.png';
+import slide1 from './../../images/about-slideshow/1.jpg';
+import slide2 from './../../images/about-slideshow/2.jpg';
+import slide3 from './../../images/about-slideshow/3.jpg';
+import slide4 from './../../images/about-slideshow/4.jpg';
+import slide5 from './../../images/about-slideshow/5.jpg';
+import slide6 from './../../images/about-slideshow/6.jpg';
 
 const aboutSlideshow = [
     {
