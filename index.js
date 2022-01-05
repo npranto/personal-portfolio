98ac12606bdd34fd70622dfffef1e07a89b3f734
Nazmuz Pranto <npranto@gmail.com>
2022-01-04 19:55:03 -0500

    adds interactivity to open/close menu listitem

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 console.log('Welcome to Index!');
+
+// elements
+const menuIconBtn = document.querySelector('#menu-icon-btn');
+const closeMenuIconBtn = document.querySelector('#close-menu-icon-btn');
+const navMenu = document.querySelector('#nav-menu');
+
+// events
+menuIconBtn.addEventListener('click', () => {
+  navMenu.classList.remove('hidden');
+  menuIconBtn.classList.add('hidden');
+  closeMenuIconBtn.classList.remove('hidden');
+  closeMenuIconBtn.classList.add('inline-block');
+});
+
+closeMenuIconBtn.addEventListener('click', () => {
+  navMenu.classList.add('hidden');
+  menuIconBtn.classList.remove('hidden');
+  closeMenuIconBtn.classList.add('hidden');
+});
