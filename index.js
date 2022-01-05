e376b9cc598c493739f49cb4130f55613e1537a0
Nazmuz Pranto <npranto@gmail.com>
2022-01-05 12:16:55 -0500

    adds UI for about and jumbotron intro

--- index.js
+++ index.js
@@ -5,16 +5,39 @@ const menuIconBtn = document.querySelector('#menu-icon-btn');
 const closeMenuIconBtn = document.querySelector('#close-menu-icon-btn');
 const navMenu = document.querySelector('#nav-menu');
 
-// events
-menuIconBtn.addEventListener('click', () => {
+function closeNavigationMenu() {
+  navMenu.classList.add('hidden');
+  menuIconBtn.classList.remove('hidden');
+  closeMenuIconBtn.classList.add('hidden');
+}
+function openNavigationMenu() {
   navMenu.classList.remove('hidden');
   menuIconBtn.classList.add('hidden');
   closeMenuIconBtn.classList.remove('hidden');
   closeMenuIconBtn.classList.add('inline-block');
-});
+}
 
-closeMenuIconBtn.addEventListener('click', () => {
-  navMenu.classList.add('hidden');
-  menuIconBtn.classList.remove('hidden');
-  closeMenuIconBtn.classList.add('hidden');
-});
+// events
+menuIconBtn.addEventListener('click', openNavigationMenu);
+closeMenuIconBtn.addEventListener('click', closeNavigationMenu);
+
+// ========== //
+
+const readMoreBtn = document.querySelector('#read-more-btn');
+const collapseReadMoreBtn = document.querySelector('#collapse-read-more-btn');
+const readMoreContent = document.querySelector('#read-more-content');
+
+function openReadMoreContent() {
+  readMoreBtn.classList.add('hidden');
+  collapseReadMoreBtn.classList.remove('hidden');
+  readMoreContent.classList.remove('hidden');
+}
+
+function closeReadMoreContent() {
+  readMoreBtn.classList.remove('hidden');
+  collapseReadMoreBtn.classList.add('hidden');
+  readMoreContent.classList.add('hidden');
+}
+
+readMoreBtn.addEventListener('click', openReadMoreContent);
+collapseReadMoreBtn.addEventListener('click', closeReadMoreContent);
