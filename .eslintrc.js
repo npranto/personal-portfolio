247f7f80f8c957559170cdd972838b2eafb1c213
Nazmuz Pranto <npranto@gmail.com>
2022-01-14 06:35:19 -0500

    adds cypress and e2e tests to run locally for now

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,12 +2,14 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    'cypress/globals': true,
   },
-  extends: ['airbnb-base', 'prettier'],
+  extends: ['airbnb-base', 'prettier', 'plugin:cypress/recommended'],
   parserOptions: {
     ecmaVersion: 13,
     sourceType: 'module',
   },
+  plugins: ['cypress'],
   rules: {
     'no-console': 'off',
     'import/extensions': 0,
