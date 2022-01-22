d9a3d1f6bafafa517717e5c43963c88451de7a25
Nazmuz Pranto <npranto@gmail.com>
2022-01-21 22:54:07 -0500

    adds eslint, prettier and editorconfig; adds LICENSE

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'prettier',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'no-console': 0,
+    'import/extensions': 0,
+    'react/jsx-filename-extension': 0,
+    'react/no-unescaped-entities': 0,
+  },
+};
---
+++ SETUP.md
@@ -0,0 +1,5 @@
+# Setup
+
+- create a new next.js app instance w/ create-next-app - `npx create-next-app@latest <PROJECT_NAME>`
+- setup TailwindCSS w/in Next.js app - https://tailwindcss.com/docs/guides/nextjs
+- 
