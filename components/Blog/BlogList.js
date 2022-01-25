8d343213b4579986bf7e16b27ebdca9c0fa65be7
Nazmuz Pranto <npranto@gmail.com>
2022-01-25 11:15:40 -0500

    add How I Setup ESLint, Prettier, and EditorConfig for Static Sites post under Blog section

--- components/Blog/BlogList.js
+++ components/Blog/BlogList.js
@@ -4,6 +4,11 @@ export default function BlogList({ className = '' }) {
 	return (
 		<ul className={className}>
 			<BlogItem
+				title="How I Setup ESLint, Prettier, and EditorConfig for Static Sites"
+				published="Jan 25, 2022"
+				href="https://dev.to/npranto/how-i-setup-eslint-prettier-and-editorconfig-for-static-sites-33ep"
+			/>
+			<BlogItem
 				title="macOS Web Development Setup Guide"
 				published="Dec 8, 2021"
 				href="https://medium.com/@npranto/macos-web-development-setup-guide-db229bcbb849"
