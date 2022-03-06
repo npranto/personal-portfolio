84f3c07c1ba63aa30195a3f3c5366b27d4967d6c
Nazmuz Pranto <npranto@gmail.com>
2022-03-06 17:33:12 -0500

    updates build process

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,8 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('./src/assets');
   eleventyConfig.addPassthroughCopy('./src/styles');
-  eleventyConfig.addPassthroughCopy('./src/vendor/fonts');
-  // eleventyConfig.addPassthroughCopy('./src/vendor/tailwind');
+  eleventyConfig.addPassthroughCopy('./src/scripts');
+  eleventyConfig.addPassthroughCopy('./src/vendor');
   eleventyConfig.addPassthroughCopy('./src/manifest.webmanifest');
 
   eleventyConfig.addWatchTarget('./src/');
